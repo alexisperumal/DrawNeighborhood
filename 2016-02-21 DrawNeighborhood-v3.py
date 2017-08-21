ba2e8777042e8c8fdd8b23cade82b1ff93e7d15f
alexisperumal <alexis.perumal@gmail.com>
2017-08-20 18:09:22 -0700

    More comment changes.

--- 2016-02-21 DrawNeighborhood-v3.py
+++ 2016-02-21 DrawNeighborhood-v3.py
@@ -26,6 +26,7 @@ def drawTriangle(x, y, scale=1, restore=False):
 # Adding a second useless comment.
 # Comment added in the branch. Boo!
 # Adding another comment with Devin at Starbucks (8/20/17)
+# Local commment conflicting with the server side comment. (8/20/17)
 
     turtle.begin_fill()
     turtle.pendown()
